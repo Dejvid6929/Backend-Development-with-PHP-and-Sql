ef9128ec8820860abde5211f28f9c244df80c855
Dejvid6929 <166691641+Dejvid6929@users.noreply.github.com>
2024-05-14 15:35:15 +0200

    Update database.php

--- Basic_Login_Page/database.php
+++ Basic_Login_Page/database.php
@@ -1,8 +1,8 @@
 <?php
-    // businessdb
+    // Business_DB
     $dbserver = "localhost";
-    $db_user = "root";
-    $db_pass = "";
+        $db_user = "root";
+        $db_pass = "";
     $db_name = "login_info";
     
     $conn = mysqli_connect($dbserver, $db_user, $db_pass, $db_name);
